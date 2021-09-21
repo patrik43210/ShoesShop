c7b4c995c7e948eacfc3525d370d94d8afcc1cd2
patrik43210 <67583747+patrik43210@users.noreply.github.com>
2021-09-21 12:38:57 +0100

    update

--- config/firebase.js
+++ config/firebase.js
@@ -1,4 +1,11 @@
 var firebaseConfig = {
-    //config
+  apiKey: "AIzaSyAILA_9GM7nV71gMzoe3acPOXTPdxj02Yw",
+  authDomain: "shoes-shop-475f4.firebaseapp.com",
+  databaseURL: "https://shoes-shop-475f4-default-rtdb.firebaseio.com",
+  projectId: "shoes-shop-475f4",
+  storageBucket: "shoes-shop-475f4.appspot.com",
+  messagingSenderId: "742828358484",
+  appId: "1:742828358484:web:77d992750afb83b51466c1",
+  measurementId: "G-87RKZ101K2",
 };
 firebase.initializeApp(firebaseConfig);
