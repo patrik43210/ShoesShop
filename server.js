1317b1411b4f0c8265c17f553f72340b87bc30f1
patrik43210 <67583747+patrik43210@users.noreply.github.com>
2021-09-21 16:05:05 +0100

    update

---
+++ server.js
@@ -0,0 +1,44 @@
+import { getHome } from "./controllers/home.js";
+import {
+  getLogin,
+  getRegister,
+  postRegister,
+  postLogin,
+  getLogout,
+} from "./controllers/user.js";
+import {
+  getCreate,
+  postCreate,
+  getDetail,
+  getEdit,
+  postEdit,
+  getDelete,
+  buy,
+} from "./controllers/shoe.js";
+
+const app = Sammy("body", function () {
+  this.use("Handlebars", "hbs");
+
+  this.get("#/home", getHome);
+
+  this.get("#/login", getLogin);
+  this.get("#/register", getRegister);
+
+  this.post("#/register", postRegister);
+  this.post("#/login", postLogin);
+  this.get("#/logout", getLogout);
+
+  this.get("#/create", getCreate);
+  this.post("#/create", postCreate);
+
+  this.get("#/details/:id", getDetail);
+
+  this.get("#/edit/:id", getEdit);
+  this.post("#/edit/:id", postEdit);
+
+  this.get("#/delete/:id", getDelete);
+
+  this.get("#/buy/:id", buy);
+});
+
+app.run("#/home");
